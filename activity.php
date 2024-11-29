fe66610217c9a55108a0002f4bb583374f44ed84
Hsinwen <wc5894@gmail.com>
2024-11-29 11:49:08 +0800

    加上activity避免檔案重名

--- activity.php
+++ activity.php
@@ -218,27 +218,6 @@ if (isset($_GET["search"])) {
 
     <title>服務項目</title>
 
-    <!-- Custom fonts for this template -->
-    <!-- <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet"> -->
-
-    <!-- Custom styles for this template -->
-    <!-- <link href="css/sb-admin-2.min.css" rel="stylesheet"> -->
-
-    <!-- Custom styles for this page -->
-    <!-- <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet"> -->
-
-
-    <!-- fontawesome -->
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
-        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
-
-    <!-- bootstrap5 -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
-
     <!-- 統一的css -->
      <?php include "css.php"; ?>
 
